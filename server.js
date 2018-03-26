f2ddbce3eb717950a00404cae4dad2d78f5bd83e
Glitch (fcc-short-url) <none>
2018-03-26 16:36:38 +0000

    💂🏥 Checkpoint ./server.js:704354/323

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error: '))
-      
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -22,7 +22,6 @@ app.use(express.static('public'))
 app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
-
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
