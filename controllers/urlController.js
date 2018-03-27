6037e587db996b20dcd200629ee0e00f867febd3
Glitch (fcc-short-url) <none>
2018-03-27 04:36:10 +0000

    🌆⚾️ Checkpoint ./controllers/urlController.js:704354/15

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,7 +1,7 @@
 var URL = require('../models/url')
 
 exports.url = function(req, res){
-  res.send('requested url'+req.params.url)
+  res.send('requested url')
 }
 
 exports.web = function(req, res){
