1e53d0e6ef946d78135c95b39f4fcc883a171fb9
Glitch (fcc-short-url) <none>
2018-03-27 06:37:08 +0000

    🌚🌒 Checkpoint ./controllers/urlController.js:704354/1575

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,16 +1,16 @@
 var URL = require('../models/url')
 var { query, validationResult } = require('express-validator/check')
 
-exports.url = function(req, res, next){
+exports.url = [
   //validation
-  query('query').isURL().withMessage('You did not pass a valid url')
-  //response
-  var errors = validationResult(req);
+  query('query').isURL().withMessage('You did not pass a valid url'),
+  (req, res, next) => {
+      var errors = validationResult(req);
   var url = new URL({
     url: req.query.query
   })
   if(!errors.isEmpty()){
-    res.json({error: 'has error'})
+    res.json({error: errors.array()[0].msg})
   }
   else{
     URL.findOne({url: req.query.query})
@@ -22,12 +22,14 @@ exports.url = function(req, res, next){
       else{
           url.save(function(err, url){
             if(err) {return next(err)}
-            res.send('short url = https://fcc-short-url.glitch.me/'+url.id )
+            res.json({'new-short-url':"https://fcc-short-url.glitch.me/"+url.id, errors: errors} )
           })
       }
     })
   }
-}
+  }
+]
+
 
 exports.web = function(req, res, next){
   URL.findById(req.params.id)
